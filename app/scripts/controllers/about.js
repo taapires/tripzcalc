81666e4d6dfa7e7b43405dd298270d5166ee7ec6
Telmo Pires <telmo.pires@gmail.com>
2014-09-08 22:30:02 +0100

    meh

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tripzcalcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tripzcalcApp
+ */
+angular.module('tripzcalcApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
